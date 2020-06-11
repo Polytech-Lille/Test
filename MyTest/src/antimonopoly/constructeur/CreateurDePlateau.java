b2db282a7b3eef47649ce7884fa902913abee840
Santiago Bragagnolo <santiagobragagnolo@gmail.com>
2020-06-11 14:10:06 +0200

    Parametre arbitraire

--- MyTest/src/antimonopoly/constructeur/CreateurDePlateau.java
+++ MyTest/src/antimonopoly/constructeur/CreateurDePlateau.java
@@ -6,7 +6,7 @@ import antimonopoly.entities.Plateau;
 public class CreateurDePlateau {
 
 	public Plateau novueauPlateau() {
-		Plateau plateau = new Plateau();
+		Plateau plateau = new Plateau(4);
 		this.configurePlateauNeoLiberal(plateau);
 		return plateau;
 	}
